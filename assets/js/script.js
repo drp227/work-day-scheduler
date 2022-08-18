d68c79e6babcdb2a3fdc38d2923647ecba654606
drp227 <drp22786@hotmail.com>
2022-08-17 22:51:20 -0500

    save button function and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,4 +19,21 @@ $(document).ready(function () {
             $(this).addClass("past");
         }
     });
+    
+    $(".saveBtn").click(function (event) {
+        event.preventDefault();
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id").split("-")[1];
+        localStorage.setItem(time, value);
+    });
+
+    $("#hour-09 .description").val(localStorage.getItem("09"));
+    $("#hour-10 .description").val(localStorage.getItem("10"));
+    $("#hour-11 .description").val(localStorage.getItem("11"));
+    $("#hour-12 .description").val(localStorage.getItem("12"));
+    $("#hour-13 .description").val(localStorage.getItem("13"));
+    $("#hour-14 .description").val(localStorage.getItem("14"));
+    $("#hour-15 .description").val(localStorage.getItem("15"));
+    $("#hour-16 .description").val(localStorage.getItem("16"));
+    $("#hour-17 .description").val(localStorage.getItem("17"));
 });
